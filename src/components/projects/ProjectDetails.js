e26d7fb63f86e73221284e6d88b840e464adc2ef
Safnas <iamsafnas@gmail.com>
2020-09-08 23:01:27 +0530

    added project details page

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function ProjectDetails() {
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title</span>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+            voluptate, optio doloribus incidunt explicabo iusto neque ab eveniet
+            magnam dicta tempore consectetur totam blanditiis quibusdam
+            voluptatum vero, eos aliquam enim.
+          </p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by Safnas</div>
+          <div>2nd September, 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+}
