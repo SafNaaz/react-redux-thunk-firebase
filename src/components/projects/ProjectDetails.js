7adad278254e20d65c2050e667417600916e4aef
Safnas <iamsafnas@gmail.com>
2020-09-08 23:17:49 +0530

    got url param in project details page

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-export default function ProjectDetails() {
+export default function ProjectDetails(props) {
+  const id = props.match.params.id;
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title</span>
+          <span className="card-title">Project Title - {id}</span>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
             voluptate, optio doloribus incidunt explicabo iusto neque ab eveniet
