c13f5a71e4b1b57a01b2a5523a74e82243d8589e
Safnas <iamsafnas@gmail.com>
2020-09-10 00:41:16 +0530

    showing project details using firestore data

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,24 +1,51 @@
 import React from "react";
+import {connect} from 'react-redux'
+import { firestoreConnect} from 'react-redux-firebase'
+import {compose} from 'redux'
 
-export default function ProjectDetails(props) {
-  const id = props.match.params.id;
-  return (
+function ProjectDetails(props) {
+  // console.log(props)
+  const {project} = props
+  if(project){
+    return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title - {id}</span>
+          <span className="card-title">{project.title}</span>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
-            voluptate, optio doloribus incidunt explicabo iusto neque ab eveniet
-            magnam dicta tempore consectetur totam blanditiis quibusdam
-            voluptatum vero, eos aliquam enim.
+            {project.content}
           </p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
-          <div>Posted by Safnas</div>
+          <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
           <div>2nd September, 2am</div>
         </div>
       </div>
     </div>
-  );
+    )
+  } else{
+    return (
+    <div className="container center">
+      <p>Loading Project...</p>
+    </div>
+    )
+  }
 }
+  
+
+const mapStateToProps = (state, ownProps) =>{
+  // console.log(state)
+  const id = ownProps.match.params.id
+  const projects =  state.fireStore.data.projects
+  const project = projects ? projects[id] : null
+  return{
+    project : project
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    {collection:'projects'}
+  ])
+)(ProjectDetails)
