433d0ec6c2f72508aca0ae2f66b55050a6f8031f
Safnas <iamsafnas@gmail.com>
2020-10-14 17:21:27 +0530

    added redux dev tools

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose  } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
@@ -14,10 +14,26 @@ import firebase from "./config/fbconfig";
 import { createFirestoreInstance } from "redux-firestore";
 import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk.withExtraArgument({ getFirebase }))
+const middleware = [thunk.withExtraArgument({ getFirebase })]
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
 );
+const store = createStore(rootReducer, enhancer);
+
+//const store = createStore(
+  //rootReducer,
+  //applyMiddleware(thunk.withExtraArgument({ getFirebase })),
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//);
 
 const rrfProps = {
   firebase,
